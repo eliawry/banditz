1cd9e39cae37960738bad06d80d546a92c5b4a5d
eli feasley <e.feasley@gmail.com>
2015-07-09 14:28:11 +0200

    mvc rewrite

--- js/main.js
+++ js/main.js
@@ -1,74 +1,119 @@
+$(function(){
+    var model = {
+        init: function() {
+          if (!localStorage.data) {
+            var data = {};
+            data.numSlotMachines = 5;
+            data.slotMachines = [];
+            for (var i = 0; i < data.numSlotMachines; i++) {
+              data.slotMachines.push({
+                'id': i,
+                'payoffProbability': Math.random(),
+                'payoffAmount': Math.floor(Math.random() * 100),
+                'points': 0,
+                'clicks': 0
+              });
+            }
+            localStorage.data = JSON.stringify(data);
+          }
+        },
+        addPoints: function(points, i) {
+            var data = JSON.parse(localStorage.data);
+            data.slotMachines[i].points += points;
+            localStorage.data = JSON.stringify(data);
+        },
+        allSlotMachines: function() {
+          return JSON.parse(localStorage.data).slotMachines;
+        },
+        slotMachine: function(i){
+          return JSON.parse(localStorage.data).slotMachines[i];
+        }
+    };
 
-var numSlotMachines = 5;
-//todo: This should probably be done with classes but ehhhh
-//if i was being a good person i'd use react anyway
-var earnedSoFar = Array.apply(null, Array(numSlotMachines)).map(Number.prototype.valueOf,0);
-var payoffProbability = [];
-var payoffAmount = [];
-for (var i = 0; i < numSlotMachines; i++) {
-    payoffProbability.push(Math.random());
-    payoffAmount.push(Math.floor(Math.random() * 100))
-}
+    var controller = {
+        spin: function(i) {
+          var machine = model.slotMachine(i);
+          if (Math.random() < machine.payoffProbability) {
+              model.addPoints(machine.payoffAmount, i);
+             }
+            view.render();
+        },
 
-// Generate a slot machine div
-var slotMachineGenerator = function(i) {
-  var slotContainer,
-      slotStuffContainer,
-      slotImageContainer,
-      slotImage,
-      slotName,
-      pointCountContainer,
-      pointCount;
-  slotStuffContainer  = document.createElement("div");
-  slotContainer  = document.createElement("div");
-  slotImageContainer = document.createElement("div");
-  slotImage = document.createElement("img");
-  pointCountContainer = document.createElement("div");
-  pointCount = 0;
+        init: function() {
+            model.init();
+            view.init();
+        },
 
-  slotContainer.classList.add("slotMachineContainer");
-  var width = 100.0 / numSlotMachines; 
-  slotStuffContainer.style.width = width.toString() +  "%";
-  slotStuffContainer.style.height = "325px";
-  slotStuffContainer.id = "slot" + i;
+        getSlotMachines: function() {
+          return model.allSlotMachines();
+        },
 
-  slotName = document.createElement("h4");
-  slotName.innerHTML = "<br>slot machizzle " + i.toString();
-  slotContainer.appendChild(slotName);
-  
-  pointCountContainer = document.createElement("h4");
-  pointCountContainer.id = "points" + i.toString();
-  pointCountContainer.innerHTML = "points" + pointCount.toString();
-  slotContainer.appendChild(pointCountContainer);
+        numSlotMachines: function() {
+          return model.allSlotMachines().length;
+        }
+    };
 
-  slotImage.src = "img/slots" + i.toString() + ".png";
-  slotImage.classList.add("img-responsive");
-  slotImageContainer.appendChild(slotImage);
-  slotContainer.appendChild(slotImageContainer);
+    var view = {
+        slotMachineGenerator: function(machine) {
+          var i = machine.id;
+          var name = (machine.id + 1).toString();
+          var slotContainer,
+              slotStuffContainer,
+              slotImageContainer,
+              slotImage,
+              slotName,
+              pointCountContainer,
+              pointCount;
+          slotStuffContainer  = document.createElement("div");
+          slotContainer  = document.createElement("div");
+          slotImageContainer = document.createElement("div");
+          slotImage = document.createElement("img");
+          pointCountContainer = document.createElement("div");
 
-  slotStuffContainer.classList.add("slotMachineStuffContainer");
-  slotStuffContainer.appendChild(slotContainer);
-  slotStuffContainer.pointCount = 0;
+          slotContainer.classList.add("slotMachineContainer");
+          slotStuffContainer.id = "slot" + i;
+          var width = 100.0 / controller.numSlotMachines(); 
+          slotStuffContainer.style.width = width.toString() +  "%";
 
+          slotName = document.createElement("h4");
+          slotName.innerHTML = "<br>slot machizzle " + (i + 1).toString();
+          slotContainer.appendChild(slotName);
+          
+          pointCountContainer = document.createElement("h4");
+          pointCountContainer.id = "points" + i.toString();
+          pointCountContainer.innerHTML = "points 0";
+          slotContainer.appendChild(pointCountContainer);
 
-  return slotStuffContainer;
-};
+          slotImage.src = "img/slots" + name + ".png";
+          slotImage.classList.add("img-responsive");
+          slotImageContainer.appendChild(slotImage);
+          slotContainer.appendChild(slotImageContainer);
 
+          slotStuffContainer.classList.add("slotMachineStuffContainer");
+          slotStuffContainer.appendChild(slotContainer);
 
-function addSlotMachine(i) {
-  var slotMachine = slotMachineGenerator(i);
-  slotMachine.addEventListener('click', (function(machineNum) {
-        return function() {
-            slotMachine.pointCount += Math.random() < payoffProbability[i] 
-              ? payoffAmount[i]
-              : 0;
-            $("#points" + i.toString())[0].innerHTML = "points " + slotMachine.pointCount;
-        };
-    })(i));
-  $("#slots").append(slotMachine); 
-}
-
-window.onload = function() {
-    for (var i = 1; i <= numSlotMachines; i++) {
-        addSlotMachine(i);
-    }}
+          return slotStuffContainer;
+        },
+        init: function() {
+            var slotMachines = controller.getSlotMachines();
+            [].forEach.call(slotMachines, (function(m){
+              var machine = view.slotMachineGenerator(m);
+              machine.addEventListener('click', (function(machineNum) {
+                    return function() {
+                      controller.spin(machineNum);
+                      view.render();
+                    };
+                })(m.id));
+              $("#slots").append(machine);
+            }));
+            view.render();
+        },
+        render: function(){
+            var slotMachines = controller.getSlotMachines();
+            [].forEach.call(slotMachines, function(machine) {
+              $("#points" + machine.id.toString())[0].innerHTML = "points " + machine.points;
+            })
+        }
+    };
+    controller.init();
+});
